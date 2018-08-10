c94f54fdd3e9047a3cf56a919e0e0d494af33c7f
4nc3str4l <cmuri_10@hotmail.com>
2018-08-11 01:00:48 +0200

    Empty Project and some art

---
+++ desktop/src/net/lostsocket/ld42/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package net.lostsocket.ld42.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.lostsocket.ld42.RunningOutOfSpace;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 960;
+		config.height = 640;
+		new LwjglApplication(new RunningOutOfSpace(), config);
+	}
+}
