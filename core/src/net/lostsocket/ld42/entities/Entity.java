17a585b517c9ba994042741692f22e230240a604
4nc3str4l <cmuri_10@hotmail.com>
2018-08-11 01:07:49 +0200

    Entity and Scene Classes

---
+++ core/src/net/lostsocket/ld42/entities/Entity.java
@@ -0,0 +1,14 @@
+package net.lostsocket.ld42.entities;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public abstract class Entity 
+{
+	public void tick(float delta) {
+		
+	}
+	
+	public void render(SpriteBatch batch) {
+		
+	}
+}
---
+++ core/src/net/lostsocket/ld42/scenes/AbstractScene.java
@@ -0,0 +1,24 @@
+package net.lostsocket.ld42.scenes;
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import net.lostsocket.ld42.entities.Entity;
+
+public abstract class AbstractScene {
+	
+	public ArrayList<Entity> entities = new ArrayList<Entity>();
+	
+	public void tick(float delta) {
+		for(Entity entity : entities) {
+			entity.tick(delta);
+		}
+	}
+	
+	public void render(SpriteBatch batch){
+		for(Entity entity : entities) {
+			entity.render(batch);
+		}
+	}
+}
