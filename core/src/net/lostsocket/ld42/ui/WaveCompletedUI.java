e3b7b48d9583d7b380f08f4363c4575ab8a0a560
4nc3str4l <cmuri_10@hotmail.com>
2018-08-11 18:40:55 +0200

    Button Sprites

---
+++ core/src/net/lostsocket/ld42/ui/WaveCompletedUI.java
@@ -0,0 +1,16 @@
+package net.lostsocket.ld42.ui;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class WaveCompletedUI extends UI {
+
+	@Override
+	public void render(SpriteBatch _batch) {
+		
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+}
